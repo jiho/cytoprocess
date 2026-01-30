ec98642007a212791d33bf747e8bf7a7443e6ee7
Jean-Olivier Irisson <irisson@normalesup.org>
2026-01-30 14:17:49 +0100

    Use the more specific project keyword

--- cytoprocess/commands/create.py
+++ cytoprocess/commands/create.py
@@ -1,20 +1,23 @@
 import logging
 import os
+from pathlib import Path
 
 
-def run(ctx, directory):
+def run(ctx, project):
     logger = logging.getLogger("cytoprocess.create")
-    logger.info(f"create project {directory}")
+    logger.info(f"Create project {project}")
     logger.debug("Context: %s", getattr(ctx, "obj", {}))
     
     # Create the main directory if it doesn't exist
-    os.makedirs(directory, exist_ok=True)
+    if (Path(project).exists()):
+        logger.info(f"Project directory {project} already exists." )
+    os.makedirs(project, exist_ok=True)
     
     # List of subdirectories to create
     subdirectories = ["raw", "converted", "work", "config", "meta", "ecotaxa"]
     
     # Create each subdirectory
     for subdir in subdirectories:
-        subdir_path = os.path.join(directory, subdir)
+        subdir_path = os.path.join(project, subdir)
         os.makedirs(subdir_path, exist_ok=True)
         logger.debug(f"Created subdirectory: {subdir_path}")
